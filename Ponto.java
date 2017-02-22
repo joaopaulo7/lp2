eba645d82e7477d864f6d213c237a6d33103996f
alunos <joaopauloff7@hotmail.comgit config --global user.email joaopauloff7@hotmail.com>
2017-02-22 10:32:32 -0300

    Autualização Problema:Pontos- Uso de teste mais complexo.

--- Ponto.java
+++ Ponto.java
@@ -17,11 +17,11 @@ public class Ponto{
 
 	public static void main(String[] args){
 		Ponto p1 = new Ponto();
-		p1.setX(0.0);
-		p1.setY(3.0);
+		p1.setX(7.0);
+		p1.setY(2.0);
 		Ponto p2 = new Ponto();
-		p2.setX(4.0);
-		p2.setY(0.0);
+		p2.setX(3.0);
+		p2.setY(5.0);
 		System.out.println(Math.sqrt(Math.pow((p1.getX()-p2.getX()),2)+Math.pow((p1.getY()-p2.getY()),2)));
 	}
 }
