98fb81a43ecd93a0750009fc95ee77df27540770
joão Paulo <joaopauloff7@hotmail.com>
2017-03-11 13:00:07 -0300

    Aprimorados os testes foram

--- ep3/Principal.java
+++ ep3/Principal.java
@@ -4,10 +4,17 @@ import java.util.Collections;
 public class Principal{
 	public static void main(String Arg[]){
 		ArrayList<Aluno> lista = new ArrayList<Aluno>();
-		for(int i = 12; i > 3; i--)
+		for(int i = 0; i < 12; i++)
 		{
 			lista.add(new Aluno("joao", i*10));
 		}
+
+		for(int i = 0; i < 5; i++)
+                {
+                        System.out.println(lista.get(i).getIdade() +"--"+ lista.get(i).getNome());
+                }
+
+
 		Collections.sort(lista);
 		for(int i = 0; i < 5; i++)
                 {
